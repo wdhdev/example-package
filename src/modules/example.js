7a86021ce158b3c35afd43951669839224a240b8
William Harrison <william@williamdavidharrison.com.au>
2023-03-16 06:52:57 +0800

    upload package

---
+++ src/modules/example.js
@@ -0,0 +1,8 @@
+/**
+ * @function `example` - An example module.
+ * @returns { void } - An instance of console.log().
+ */
+
+module.exports = function example() {
+    return console.log("Hello World");
+}
